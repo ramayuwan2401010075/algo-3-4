b99b1e582080978597634ff700a9ff96948194a6
ramayuwan2401010075 <ramayuwan099@gmail.com>
2024-12-05 12:17:18 +0800

    Add files via upload

---
+++ pert 501.py
@@ -0,0 +1,5 @@
+kalimat ="rama"
+print("kalimat : ", kalimat)
+print("\"a\" in kalimat :","a" in kalimat)
+print("\"q\" in kalimat :","q" in kalimat)
+print("\"l\" in kalimat :","l"  not in kalimat)
---
+++ pert 502.py
@@ -0,0 +1,6 @@
+name = "rama belajar"
+
+print("name[0] : ", name[0])
+print("name[0:3]: ", name[0:3])
+print("name[1:4]: ", name[1:4])
+print("name[1:7]: ", name[1:7])
